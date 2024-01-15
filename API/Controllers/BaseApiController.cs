409834da17892474ec732db21658c0b7725afb30
Ardavan Shafiee <bm.bigbang@gmail.com>
2024-01-15 17:02:43 +0000

    Add register user controller Refactor base api functionality Add user password database details

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers {
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
